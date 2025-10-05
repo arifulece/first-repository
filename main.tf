50563cc5a2c63b29b60cd21879ef4128ff260328
arifulece <arifulece1219@gmail.com>
2025-10-05 10:07:42 +0600

    terraform nginx & apache code

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull Nginx image
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# Run Nginx container
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id   # <-- fix here
+  name  = "terraform-nginx"
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
+
+# APACHE Image & Container
+resource "docker_image" "apache" {
+  name         = "httpd:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "apache" {
+  image = docker_image.apache.image_id
+  name  = "terraform-apache"
+
+  ports {
+    internal = 80
+    external = 8082
+  }
+}
